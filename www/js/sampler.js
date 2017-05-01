f0ae348f5bdd4ef2124534deb95ecf8d9155c07d
TommyNT <tommynt@outlook.com>
2017-05-01 13:38:19 +0200

    other pads sample added

--- www/js/sampler.js
+++ www/js/sampler.js
@@ -12,8 +12,8 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-	// var audio = new Audio('file:///android_asset/www/samples/Slice - 002.wav');
-	var audio = new Audio('./samples/Slice - 006.wav');
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 003.wav');
+	var audio = new Audio('./samples/Slice - 003.wav');
 	var playing = false;
 	$( "#sample2" ).on( "click", function (){
 		if (playing == false) {
@@ -25,8 +25,8 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-	// var audio = new Audio('file:///android_asset/www/samples/Slice - 002.wav');
-	var audio = new Audio('./samples/Slice - 007.wav');
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 004.wav');
+	var audio = new Audio('./samples/Slice - 004.wav');
 	var playing = false;
 	$( "#sample3" ).on( "click", function (){
 		if (playing == false) {
@@ -38,10 +38,75 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-	// var audio = new Audio('file:///android_asset/www/samples/Slice - 002.wav');
-	var audio = new Audio('./samples/Slice - 008.wav');
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 005.wav');
+	var audio = new Audio('./samples/Slice - 005.wav');
 	var playing = false;
 	$( "#sample4" ).on( "click", function (){
+		if (playing == false) {
+			audio.play();
+			playing = false;
+			audio.currentTime = 0;
+		}
+	});
+});
+
+$(document).ready(function () {
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 006.wav');
+	var audio = new Audio('./samples/Slice - 006.wav');
+	var playing = false;
+	$( "#sample5" ).on( "click", function (){
+		if (playing == false) {
+			audio.play();
+			playing = false;
+			audio.currentTime = 0;
+		}
+	});
+});
+
+$(document).ready(function () {
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 007.wav');
+	var audio = new Audio('./samples/Slice - 007.wav');
+	var playing = false;
+	$( "#sample6" ).on( "click", function (){
+		if (playing == false) {
+			audio.play();
+			playing = false;
+			audio.currentTime = 0;
+		}
+	});
+});
+
+$(document).ready(function () {
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 008.wav');
+	var audio = new Audio('./samples/Slice - 008.wav');
+	var playing = false;
+	$( "#sample7" ).on( "click", function (){
+		if (playing == false) {
+			audio.play();
+			playing = false;
+			audio.currentTime = 0;
+		}
+	});
+});
+
+$(document).ready(function () {
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 010.wav');
+	var audio = new Audio('./samples/Slice - 010.wav');
+	var playing = false;
+	$( "#sample8" ).on( "click", function (){
+		if (playing == false) {
+			audio.play();
+			playing = false;
+			audio.currentTime = 0;
+		}
+	});
+});
+
+$(document).ready(function () {
+	// var audio = new Audio('file:///android_asset/www/samples/Slice - 014.wav');
+	var audio = new Audio('./samples/Slice - 014.wav');
+	var playing = false;
+	$( "#sample9" ).on( "click", function (){
 		if (playing == false) {
 			audio.play();
 			playing = false;
