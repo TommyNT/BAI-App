836b27448e7b5832e159742ce7da1a042b9dc604
pawlo1 <pawlo1337@gmail.com>
2017-05-02 19:31:07 +0200

    onClick function

--- www/js/sampler.js
+++ www/js/sampler.js
@@ -6,70 +6,120 @@ function onDeviceReady() {
 	navigator.notification.beep(1);
 }
 
-$(document).ready(function () {
-	var audio = new Media('file:///android_asset/www/samples/Slice - 002.wav');
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 002.wav');
 	// var audio = new Audio('./samples/Slice - 002.wav');
-	var playing = false;
-	$( "#sample1" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	// var playing = false;
+	// $( "#sample1" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
 
-$(document).ready(function () {
+function sample1(){
+	var audio = new Media('file:///android_asset/www/samples/Slice - 002.wav');
+	var playing = false;
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
+
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 003.wav');
+	//var audio = new Audio('./samples/Slice - 003.wav');
+	// var playing = false;
+	// $( "#sample2" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
+
+function sample2(){
 	var audio = new Media('file:///android_asset/www/samples/Slice - 003.wav');
-	// var audio = new Audio('./samples/Slice - 003.wav');
 	var playing = false;
-	$( "#sample2" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
 
-$(document).ready(function () {
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 004.wav');
+	//var audio = new Audio('./samples/Slice - 004.wav');
+	// var playing = false;
+	// $( "#sample3" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
+
+function sample3(){
 	var audio = new Media('file:///android_asset/www/samples/Slice - 004.wav');
-	// var audio = new Audio('./samples/Slice - 004.wav');
 	var playing = false;
-	$( "#sample3" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
 
-$(document).ready(function () {
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 005.wav');
+	//var audio = new Audio('./samples/Slice - 005.wav');
+	// var playing = false;
+	// $( "#sample4" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
+
+function sample4(){
 	var audio = new Media('file:///android_asset/www/samples/Slice - 005.wav');
-	// var audio = new Audio('./samples/Slice - 005.wav');
 	var playing = false;
-	$( "#sample4" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
 
-$(document).ready(function () {
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 006.wav');
+	//var audio = new Audio('./samples/Slice - 006.wav');
+	// var playing = false;
+	// $( "#sample5" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
+
+function sample5(){
 	var audio = new Media('file:///android_asset/www/samples/Slice - 006.wav');
-	// var audio = new Audio('./samples/Slice - 006.wav');
 	var playing = false;
-	$( "#sample5" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
 
 // $(document).ready(function () {
 // 	var audio = new Media('file:///android_asset/www/samples/Slice - 007.wav');
@@ -88,47 +138,76 @@ function sample6(){
 	var audio = new Media('file:///android_asset/www/samples/Slice - 007.wav');
 	var playing = false;
 	if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
 }
 
-$(document).ready(function () {
-	var audio = new Media('file:///android_asset/www/samples/Slice - 008.wav');
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 008.wav');
 	// var audio = new Audio('./samples/Slice - 008.wav');
-	var playing = false;
-	$( "#sample7" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	// var playing = false;
+	// $( "#sample7" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
 
-$(document).ready(function () {
-	var audio = new Media('file:///android_asset/www/samples/Slice - 010.wav');
+
+function sample7(){
+	var audio = new Media('file:///android_asset/www/samples/Slice - 008.wav');
+	var playing = false;
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 010.wav');
 	// var audio = new Audio('./samples/Slice - 010.wav');
-	var playing = false;
-	$( "#sample8" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	// var playing = false;
+	// $( "#sample8" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
 
-$(document).ready(function () {
-	var audio = new Media('file:///android_asset/www/samples/Slice - 014.wav');
-	// var audio = new Audio('./samples/Slice - 014.wav');
+unction sample8(){
+	var audio = new Media('file:///android_asset/www/samples/Slice - 010.wav');
 	var playing = false;
-	$( "#sample9" ).click( function (){
-		if (playing == false) {
-			audio.play();
-			playing = false;
-			audio.seekTo(0);
-		}
-	});
-});
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
+// $(document).ready(function () {
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 014.wav');
+	// var audio = new Audio('./samples/Slice - 014.wav');
+	// var playing = false;
+	// $( "#sample9" ).click( function (){
+		// if (playing == false) {
+			// audio.play();
+			// playing = false;
+			// audio.seekTo(0);
+		// }
+	// });
+// });
+
+function sample9(){
+	var audio = new Media('file:///android_asset/www/samples/Slice - 014.wav');
+	var playing = false;
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
+}
