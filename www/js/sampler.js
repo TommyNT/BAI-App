392c7fd01e923b82f90d22c434ec49c2ed7c2c05
TommyNT <tommynt@outlook.com>
2017-05-02 22:59:18 +0200

    PGLowLatencyAudio deleted

--- www/js/sampler.js
+++ www/js/sampler.js
@@ -1,24 +1,13 @@
-
-function onBodyLoad() {		
-    document.addEventListener("deviceready", onDeviceReady, false);
-    document.addEventListener("touchmove", function (e) { e.preventDefault(); return false; }, false);
-    document.addEventListener("touchstart", function (e) { e.preventDefault(); return false; }, false);	
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
 }
 
 function onDeviceReady() {
-	PGLowLatencyAudio.preloadFX('sample1', 'samples/Slice - 002.mp3');
-	PGLowLatencyAudio.preloadFX('sample2', 'samples/Slice - 003.mp3');
-	PGLowLatencyAudio.preloadFX('sample3', 'samples/Slice - 004.mp3');
-	PGLowLatencyAudio.preloadFX('sample4', 'samples/Slice - 005.mp3');
-	PGLowLatencyAudio.preloadFX('sample5', 'samples/Slice - 006.mp3');
-	PGLowLatencyAudio.preloadFX('sample6', 'samples/Slice - 007.mp3');
-	PGLowLatencyAudio.preloadFX('sample7', 'samples/Slice - 008.mp3');
-	PGLowLatencyAudio.preloadFX('sample8', 'samples/Slice - 010.mp3');
-	PGLowLatencyAudio.preloadFX('sample9', 'samples/Slice - 014.mp3');
+	navigator.notification.beep(1);
 }
 
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 002.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 002.mp3');
 	// var audio = new Audio('./samples/Slice - 002.mp3');
 	// var playing = false;
 	// $( "#sample1" ).click( function (){
@@ -30,24 +19,18 @@ function onDeviceReady() {
 	// });
 // });
 
-// function sample1(){
-// 	var audio = new Media('samples/Slice - 002.mp3');
-// 	var playing = false;
-// 	if (playing == false) {
-// 		audio.play();
-// 		playing = false;
-// 		audio.seekTo(0);
-// 	}
-// }
-
-
 function sample1(){
-	document.getElementById("sample1").className = "touched";
-    PGLowLatencyAudio.play('sample1');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 002.mp3');
+	var playing = false;
+	if (playing == false) {
+		audio.play();
+		playing = false;
+		audio.seekTo(0);
+	}
 }
 
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 003.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 003.mp3');
 	//var audio = new Audio('./samples/Slice - 003.mp3');
 	// var playing = false;
 	// $( "#sample2" ).click( function (){
@@ -60,7 +43,7 @@ function sample1(){
 // });
 
 function sample2(){
-	var audio = new Media('samples/Slice - 003.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 003.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
@@ -70,7 +53,7 @@ function sample2(){
 }
 
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 004.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 004.mp3');
 	//var audio = new Audio('./samples/Slice - 004.mp3');
 	// var playing = false;
 	// $( "#sample3" ).click( function (){
@@ -83,7 +66,7 @@ function sample2(){
 // });
 
 function sample3(){
-	var audio = new Media('samples/Slice - 004.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 004.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
@@ -93,7 +76,7 @@ function sample3(){
 }
 
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 005.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 005.mp3');
 	//var audio = new Audio('./samples/Slice - 005.mp3');
 	// var playing = false;
 	// $( "#sample4" ).click( function (){
@@ -106,7 +89,7 @@ function sample3(){
 // });
 
 function sample4(){
-	var audio = new Media('samples/Slice - 005.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 005.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
@@ -116,7 +99,7 @@ function sample4(){
 }
 
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 006.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 006.mp3');
 	//var audio = new Audio('./samples/Slice - 006.mp3');
 	// var playing = false;
 	// $( "#sample5" ).click( function (){
@@ -129,7 +112,7 @@ function sample4(){
 // });
 
 function sample5(){
-	var audio = new Media('samples/Slice - 006.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 006.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
@@ -139,7 +122,7 @@ function sample5(){
 }
 
 // $(document).ready(function () {
-// 	var audio = new Media('samples/Slice - 007.mp3');
+// 	var audio = new Media('file:///android_asset/www/samples/Slice - 007.mp3');
 // 	// var audio = new Audio('./samples/Slice - 007.mp3');
 // 	var playing = false;
 // 	$( "#sample6" ).click( function (){
@@ -152,7 +135,7 @@ function sample5(){
 // });
 
 function sample6(){
-	var audio = new Media('samples/Slice - 007.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 007.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
@@ -162,7 +145,7 @@ function sample6(){
 }
 
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 008.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 008.mp3');
 	// var audio = new Audio('./samples/Slice - 008.mp3');
 	// var playing = false;
 	// $( "#sample7" ).click( function (){
@@ -176,7 +159,7 @@ function sample6(){
 
 
 function sample7(){
-	var audio = new Media('samples/Slice - 008.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 008.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
@@ -185,7 +168,7 @@ function sample7(){
 	}
 }
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 010.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 010.mp3');
 	// var audio = new Audio('./samples/Slice - 010.mp3');
 	// var playing = false;
 	// $( "#sample8" ).click( function (){
@@ -198,7 +181,7 @@ function sample7(){
 // });
 
 function sample8(){
-	var audio = new Media('samples/Slice - 010.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 010.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
@@ -207,7 +190,7 @@ function sample8(){
 	}
 }
 // $(document).ready(function () {
-	// var audio = new Media('samples/Slice - 014.mp3');
+	// var audio = new Media('file:///android_asset/www/samples/Slice - 014.mp3');
 	// var audio = new Audio('./samples/Slice - 014.mp3');
 	// var playing = false;
 	// $( "#sample9" ).click( function (){
@@ -220,7 +203,7 @@ function sample8(){
 // });
 
 function sample9(){
-	var audio = new Media('samples/Slice - 014.mp3');
+	var audio = new Media('file:///android_asset/www/samples/Slice - 014.mp3');
 	var playing = false;
 	if (playing == false) {
 		audio.play();
