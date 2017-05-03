e20e9f7e831b4c5c41a1de4c88643de4071b9bf2
TommyNT <tommynt@outlook.com>
2017-05-03 21:33:45 +0200

    angular not completed

--- www/js/index.js
+++ www/js/index.js
@@ -1,79 +1,105 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        console.log('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
+var myApp = angular.module('myApp', ['ngRoute','ngCordova']);
 
-        if( window.plugins && window.plugins.NativeAudio ) {
+function init() {
+    document.addEventListener("deviceready", function () {
+        $cordovaNativeAudio.then(success, error);
+}, false);
 
-            var items = ['Slice - 002', 'snare', 'highhat', 'bongo'];
-            for(var i=0; i<items.length; i++) {
-                var asset = 'assets/' + items[i] + '.mp3';
-                window.plugins.NativeAudio.preloadSimple(items[i], 
-                                                         asset, 
-                                                         function(msg){console.info(msg)}, 
-                                                         function(msg){ console.error( 'Error: ' + msg ); });
-            }
+// myApp.controller('MyController', ['$scope', '$cordovaNativeAudio', '$timeout', function($scope, $cordovaNativeAudio, $timeout) {
 
-            window.plugins.NativeAudio.preloadComplex('noise', 
-                                                      'assets/ambient.mp3', 
-                                                      1, // volume
-                                                      1, // voices
-                                                      0, // delay
-            function(msg) {
-                console.info(msg); 
-                window.plugins.NativeAudio.play('noise', 
-                                                function(msg){console.info(msg)}, 
-                                                function(msg){ console.error( 'Error: ' + msg ); }, 
-                                                function(msg){ console.error( 'Complete: ' + msg ); });
-            }, 
-                                                      function(msg){ alert( 'Error: ' + msg ); });
+//   $cordovaNativeAudio
+//     .preloadSimple('click', 'assets/bass.mp3')
+//     .then(function (msg) {
+//       console.log(msg);
+//     }, function (error) {
+//       alert(error);
+//     });
 
-        }
+//   $cordovaNativeAudio
+//     .preloadComplex('music', 'audio/music.mp3', 1, 1)
+//     .then(function (msg) {
+//       console.log(msg);
+//     }, function (error) {
+//       console.error(error);
+//     });
 
-    },
+//   $scope.play = function () {
+//     $cordovaNativeAudio.play('click');
+//     $cordovaNativeAudio.loop('music');
 
-    play: function(drum) {
-        document.getElementById(drum).classList.add('touched');
-        window.plugins.NativeAudio.play(drum, 
-                                        function(msg){console.info(msg), document.getElementById(drum).classList.remove('touched');},
-                                        function(msg){ console.error( 'Error: ' + msg ); });
-    }
+//     // stop 'music' loop and unload
+//     $timeout(function () {
+//       $cordovaNativeAudio.stop('music');
+
+//       $cordovaNativeAudio.unload('click');
+//       $cordovaNativeAudio.unload('music');
+//     }, 1000 * 60);
+//   };
 
 
-};
+//     $scope.app = {
+//         // Application Constructor
+//         initialize: function() {
+//             this.bindEvents();
+//         },
+//         // Bind Event Listeners
+//         //
+//         // Bind any events that are required on startup. Common events are:
+//         // 'load', 'deviceready', 'offline', and 'online'.
+//         bindEvents: function() {
+//             document.addEventListener('deviceready', this.onDeviceReady, false);
+//         },
+//         // deviceready Event Handler
+//         //
+//         // The scope of 'this' is the event. In order to call the 'receivedEvent'
+//         // function, we must explicitly call 'app.receivedEvent(...);'
+//         onDeviceReady: function() {
+//             $scope.app.receivedEvent('deviceready');
+//             console.log('deviceready');
+//         },
+//         // Update DOM on a Received Event
+//         receivedEvent: function(id) {
+
+//             if( window.plugins && window.plugins.NativeAudio ) {
+
+//                 var items = ['bass', 'snare', 'highhat', 'bongo'];
+//                 for(var i=0; i<items.length; i++) {
+//                     var asset = 'assets/' + items[i] + '.mp3';
+//                     window.plugins.NativeAudio.preloadSimple(items[i], 
+//                                                              asset, 
+//                                                              function(msg){console.info(msg)}, 
+//                                                              function(msg){ console.error( 'Error: ' + msg ); });
+//                 }
+
+//                 window.plugins.NativeAudio.preloadComplex('noise', 
+//                                                           'assets/ambient.mp3', 
+//                                                           1, // volume
+//                                                           1, // voices
+//                                                           0, // delay
+//                 function(msg) {
+//                     console.info(msg); 
+//                     window.plugins.NativeAudio.play('noise', 
+//                                                     function(msg){console.info(msg)}, 
+//                                                     function(msg){ console.error( 'Error: ' + msg ); }, 
+//                                                     function(msg){ console.error( 'Complete: ' + msg ); });
+//                 }, 
+//                                                           function(msg){ alert( 'Error: ' + msg ); });
+
+//             }
+
+//         },
+
+//         play: function(drum) {
+//             document.getElementById(drum).classList.add('touched');
+//             window.plugins.NativeAudio.play(drum, 
+//                                             function(msg){console.info(msg), document.getElementById(drum).classList.remove('touched');},
+//                                             function(msg){ console.error( 'Error: ' + msg ); });
+//         }
+//     }
+
+// }]);
+
+myApp.controller('MyController', ['$scope', function($scope) {
+  $scope.timeOfDay = 'morning';
+  $scope.name = 'Nikki';
+}]);
